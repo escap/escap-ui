ef9ee26fbd0863b881ed8bdad908583421a44632
Nicola Selleri <nicola.selleri@fao.org>
2015-09-16 17:34:48 +0200

    Home news / Contributors / Labels Home / Collaboration text

--- i18n/root/home.js
+++ i18n/root/home.js
@@ -17,9 +17,9 @@ define([
         link_food_safety: "http://google.com",
         title_food_safety: "Food safety",
         link_food_nutrition: "http://google.com",
-        title_food_nutrition: "Food nutrition",
+        title_food_nutrition: "Nutrition",
         link_food_environment: "http://google.com",
-        title_food_environment: "Food environment",
+        title_food_environment: "Environment",
         database_update_title: "News",
 
         contacts_collaborations_title: "In collaboration with",
